c1eb3b620dc70d72847550d5c4a96d70669bede2
suhanz <han@suhandeMacBook-Pro.local>
2023-08-09 20:00:02 +0800

    覆盖率配置

--- tests/unit/ObjectField.spec.ts
+++ tests/unit/ObjectField.spec.ts
@@ -1,45 +1,46 @@
-// import { mount } from '@vue/test-utils'
-// import { defineComponent } from 'vue'
-// import JsonSchemaForm, { NumberField, StringField } from '../../lib'
+import { mount } from '@vue/test-utils'
+import { defineComponent } from 'vue'
+import { NumberField, StringField } from '../../lib'
+import TestComponent from './utils/TestComponent'
 
-// describe('ObjectField', () => {
-//   it('should render properties to correct fields', async () => {
-//     interface Value {
-//       name?: string
-//       age?: number
-//     }
-//     let value: Value = {
-//       name: '',
-//       age: 0,
-//     }
+describe('ObjectField', () => {
+  it('should render properties to correct fields', async () => {
+    interface Value {
+      name?: string
+      age?: number
+    }
+    let value: Value = {
+      name: '',
+      age: 0,
+    }
 
-//     const wrapper = mount(JsonSchemaForm, {
-//       props: {
-//         schema: {
-//           type: 'object',
-//           properties: {
-//             name: {
-//               type: 'string',
-//             },
-//             age: {
-//               type: 'number',
-//             },
-//           },
-//         },
-//         value: value,
-//         onChange: (v: object) => {
-//           value = v
-//         },
-//       },
-//     })
-//     const strField = wrapper.findComponent(StringField)
-//     const numberField = wrapper.findComponent(NumberField)
-//     // expect(strField.exists()).toBeTruthy()
-//     // expect(numberField.exists()).toBeTruthy()
-//     await strField.props('onChange')('lisa')
-//     console.log('value', value.name)
-//     expect(value.name).toBe('lisa')
-//     await numberField.props('onChange')(1)
-//     expect(value.age).toEqual(1)
-//   })
-// })
+    const wrapper = mount(TestComponent, {
+      props: {
+        schema: {
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string',
+            },
+            age: {
+              type: 'number',
+            },
+          },
+        },
+        value: value,
+        onChange: (v: object) => {
+          value = v
+        },
+      },
+    })
+    const strField = wrapper.findComponent(StringField)
+    const numberField = wrapper.findComponent(NumberField)
+    // expect(strField.exists()).toBeTruthy()
+    // expect(numberField.exists()).toBeTruthy()
+    await strField.props('onChange')('lisa')
+    console.log('value', value.name)
+    expect(value.name).toBe('lisa')
+    await numberField.props('onChange')(1)
+    expect(value.age).toEqual(1)
+  })
+})
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,22 +1,23 @@
-// import { mount } from '@vue/test-utils'
-// import { defineComponent, h } from 'vue'
-// import JsonSchemaForm, { NumberField } from '../../lib'
+import { mount } from '@vue/test-utils'
+import { defineComponent, h } from 'vue'
+import { NumberField } from '../../lib'
+import TestComponent from './utils/TestComponent'
 
-// describe('JsonSchemaForm', () => {
-//   it('should render correct number field', async () => {
-//     let value = ''
-//     const wrapper = mount(JsonSchemaForm, {
-//       props: {
-//         schema: {
-//           type: 'number',
-//         },
-//         value: value,
-//         onChange: (v: string) => {
-//           value = v
-//         },
-//       },
-//     })
-//     const NumberFiled1 = wrapper.findComponent(NumberField)
-//     expect(NumberFiled1.exists()).toBeTruthy()
-//   })
-// })
+describe('TestComponent', () => {
+  it('should render correct number field', async () => {
+    let value = ''
+    const wrapper = mount(TestComponent, {
+      props: {
+        schema: {
+          type: 'number',
+        },
+        value: value,
+        onChange: (v: string) => {
+          value = v
+        },
+      },
+    })
+    const NumberFiled1 = wrapper.findComponent(NumberField)
+    expect(NumberFiled1.exists()).toBeTruthy()
+  })
+})
